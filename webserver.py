4c722794ad24c7c875f9b5d87e1a462089f0e4a2
a.kratzer@kratzer.it <a.kratzer@kratzer.it>
2014-03-22 10:18:31 +0100

    cleanup

--- webserver.py
+++ webserver.py
@@ -27,38 +27,81 @@ class BinaryKitchenHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
     
 	op = {'box_size': 4, 'pic': 'pic.png', 'template': 'template.png'}
 
+	ERROR = '<html><body>YOU ARE NOT COOKING!!</body></html>'
+
 	def parse_POST(self):
 		para = dict()
 		form = cgi.FieldStorage(fp=self.rfile,headers=self.headers,environ={'REQUEST_METHOD':'POST','CONTENT_TYPE':self.headers['Content-Type'],})
 		return form
+	
+	#handle POST command
+	def do_POST(self):
+
+		form = self.parse_POST()
+
+		c = False
+		if 'c' in form:
+			c = int(form.getvalue('c'))
+
+		u = False
+		if 'u' in form:
+			u = form.getvalue('u')
+
+		p = False
+		if 'p' in form:
+			p = form.getvalue('p')
+
+		self.process( 'POST', c, u, p )
 
 	#handle GET command
-	def do_POST(self):
+	def do_GET(self):
+
+		o = urlparse(self.path)
+		para = parse_qs(o.query)
+
+		c = False
+		if 'c' in para:
+			c = int(para["c"][0])
+		u = False
+		if 'u' in para:
+			u = para["u"][0]
+		p = False
+		if 'p' in para:
+			p = para["p"][0]
+
+		self.process( 'GET', c, u, p )
+
+	def process(self):
+
 		try:
-			global rannum		
-			form = self.parse_POST()
-			#print( form )
-			#self.log_message( str(o) )
-			#send code 200 response
+			global rannum
+
 			self.send_response(200)
-		
-			if "c" not in form or "u" not in form or "p" not in form:
-				message = ''.join([
-					'<html><body><br>YOU ARE NOT COOKING!!',
-					'</body></html>',
-					])
+
+			if not c or not u or not p:
 				self.send_header('Content-type','text-html')
 				self.end_headers()
+				self.wfile.write( bytes(self.ERROR, 'UTF-8') )
+				return
+
+			if c != rannum:
+				message = self.ERROR
+
+			elif not c:
+
+				message = ''.join([
+					'<html><body><form name="login" method="post">', 
+					'User:<input type="text" name="u">',
+					'Pass:<input type="password" name="p">',
+					'<input type="hidden" name="c" value="%s">' % rannum,
+					'<input type="submit" name="login_do_login" value="Login"></form>',
+					'</body></html>',
+					])
+		
 				self.wfile.write( bytes(message, 'UTF-8') )
 				return
 
-			if int(form.getvalue("c")) != rannum:
-				message = ''.join([
-					'<html><body><br>YOU ARE NOT COOKING!!',
-					'</body></html>',
-					])
-
-			elif form.getvalue("u") == USER and form.getvalue("p") == PASS:
+			elif u == USER and p == PASS:
 
 				relais_process = subprocess.Popen('unlock3s', shell=False, stdin=subprocess.PIPE)
 				message = ''.join([
@@ -76,10 +119,7 @@ class BinaryKitchenHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
 					'<br></body></html>',
 					])
 			else:
-				message = ''.join([
-					'<html><body><br>YOU ARE NOT COOKING!!',
-					'</body></html>',
-					])
+				message = self.ERROR
 				
 			#send file content to client
 			self.send_header('Content-type','text-html')
@@ -93,86 +133,7 @@ class BinaryKitchenHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             
 		except IOError:
 			self.send_error(404, 'file not found')
-
-	#handle GET command
-	def do_GET(self):
-		try:
-			global rannum		
-
-			o = urlparse(self.path)
-			para = parse_qs(o.query)
-			#print( para )
-			#self.log_message( str(o) )
-			#send code 200 response
-			self.send_response(200)
-
-			if "/favicon.ico" not in self.path:
-				#send header first
-				self.send_header('Content-type','text-html')
-				self.end_headers()
-			
-				if "c" not in para:
-					message = ''.join([
-						'<html><body><br>YOU ARE NOT COOKING!!',
-						'</body></html>',
-						])
-					self.wfile.write( bytes(message, 'UTF-8') )
-					return
-
-				if int(para["c"][0]) != rannum:
-					message = ''.join([
-						'<html><body><br>YOU ARE NOT COOKING!!',
-						'</body></html>',
-						])
-
-				else:
-					if "u" not in para:
-						message = ''.join([
-							'<html><body><form name="login" method="post">', 
-							'User:<input type="text" name="u">',
-							'Pass:<input type="password" name="p">',
-							'<input type="hidden" name="c" value="%s">' % rannum,
-							'<input type="submit" name="login_do_login" value="Login"></form>',
-							'</body></html>',
-							])
-				
-						self.wfile.write( bytes(message, 'UTF-8') )
-						return
-
-					elif para["u"][0] == USER and para["p"][0] == PASS:
-
-						relais_process = subprocess.Popen('unlock3s', shell=False, stdin=subprocess.PIPE)
-						message = ''.join([
-							'<html><body>CLIENT VALUES:<br>',
-							'client_address=%s (%s)<br>' % (self.client_address,
-														self.address_string()),
-							'command=%s<br>' % self.command,
-							'path=%s<br>' % self.path,
-							'request_version=%s<br>' % self.request_version,
-							'',
-							'SERVER VALUES:<br>',
-							'server_version=%s<br>' % self.server_version,
-							'sys_version=%s<br>' % self.sys_version,
-							'protocol_version=%s<br>' % self.protocol_version,
-							'<br></body></html>',
-							])
-					else:
-						message = ''.join([
-							'<html><body><br>YOU ARE NOT COOKING!!',
-							'</body></html>',
-							])
-				#send file content to client
-				self.wfile.write( bytes(message, 'UTF-8') )
-				rannum = random.getrandbits(64)
-				print( rannum )
-				qr.do( "https://%s:%d?c=%d" % ( HOST, PORT, rannum ), sys.path[0], self.op )
-			
-			return
-            
-		except IOError:
-			self.send_error(404, 'file not found')
-
-
+		
 
 def serve(): 
 			
